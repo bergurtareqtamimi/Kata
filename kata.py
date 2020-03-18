db90a29b49a906c21461c8742694bb0058187218
Bergur Tareq Tamimi <bergurtareq@Bergurs-MacBook-Air.local>
2020-03-18 14:36:08 +0000

    passed two numbers

--- kata.py
+++ kata.py
@@ -1,5 +1,38 @@
+def all_is_digit(a_string):
+    a_list = list(a_string)
+    for i in a_list:
+        if not i.isdigit():
+            return False
+    return True
+
+def clean_string(numbers):
+    collection_string = ""
+    for token in numbers:
+        if token.isdigit() or token == ",":
+            collection_string += token
+
+    return collection_string
+
+def to_integer(a_list):
+    for i,token in enumerate(a_list):
+        a_list[i] = int(token)
+    return a_list
+
+########## Helper functions on the top #####
+
+
 def add(numbers):
     if numbers == "":
         return 0
-    if numbers.isdigit():
+
+    elif all_is_digit(numbers) and numbers.isdigit():
         return int(numbers)
+
+    else:
+        cleaned_string = clean_string(numbers)
+
+        number_list = cleaned_string.split(",")
+        number_list = to_integer(number_list)
+
+        return sum(number_list)
+
--- test_kata.py
+++ test_kata.py
@@ -16,4 +16,28 @@ def test_for_single_digit():
     assert kata.add("9") == 9
     assert kata.add("10") == 10
 
+def test_two_numbers():
+    assert kata.add("1,2") == 3
+    assert kata.add("2,3") == 5
+    assert kata.add("3,4") == 7
+    assert kata.add("4,5") == 9
+    assert kata.add("5,6") == 11
+    assert kata.add("6,6") == 12
+    assert kata.add("7,1") == 8
+    assert kata.add("8,9") == 17
+    assert kata.add("9,4") == 13
+    assert kata.add("10,6") == 16
+
+#def test_more_than_one_numbers():
+    #assert kata.add("1,2") == 3
+    #assert kata.add("2,2,3") == 7
+    #assert kata.add("5,2,4") == 11
+    #assert kata.add("4,4,4,4,4,3") == 25
+    #assert kata.add("2") == 2
+    #assert kata.add("6,6") == 12
+    #assert kata.add("7,1,1,1,1,1,1") == 13
+    #assert kata.add("8,6,4") == 18
+    #assert kata.add("9,3,5,2") == 19
+    #assert kata.add("10,1") == 11
+
 ## pytest <filename with the test> ###############
